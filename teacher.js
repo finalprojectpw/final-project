8f6d83361391cb4532635d92d9dba3e4a71e4419
satrianone <65944375+satrianone@users.noreply.github.com>
2020-06-02 07:36:24 +0700

    menambah data guru
    ada beberapa, namanya lucu

--- teacher.js
+++ teacher.js
@@ -53,7 +53,167 @@ var teacher = [
 		"web" : "www.pekertibudi.com"
 	},
 	
-
+	{
+		"nip"	: "987135268",
+		"nama"	: "Dr. Bernad Mahfoudz",
+		"email"	: "mah.foudz@rauzuran.com",
+		"web"	: "www.mahfoudz.com"
+	},
+	
+	{
+		"nip"	: "983435268",
+		"nama"	: "Prof. Jordy Elnino",
+		"email"	: "elnino.jordy@rauzuran.com",
+		"web"	: "www.jordy-el.com"
+	},
+	
+	{
+		"nip"	: "983435568",
+		"nama"	: "Johny Sins, M.M",
+		"email"	: "sins@rauzuran.com",
+		"web"	: "www.john-sins.com"
+	},
+	
+	{
+		"nip"	: "983446268",
+		"nama"	: "Ijonk Wahyu Saputro, S.Ag",
+		"email"	: "ijonk@rauzuran.com",
+		"web"	: "www.ijonk-uwu.com"
+	},
+	
+	{
+		"nip"	: "983666268",
+		"nama"	: "Panjul Bin Tomo, S.Pd",
+		"email"	: "ijonk@rauzuran.com",
+		"web"	: "www.ijonk-uwu.com"
+	},
+	
+	{
+		"nip"	: "973446268",
+		"nama"	: "Ozawa Min Ladem, M.Pd",
+		"email"	: "ladem@rauzuran.com",
+		"web"	: "www.ozawaladem.com"
+	},
+	
+	{
+		"nip"	: "983446268",
+		"nama"	: "Ijonk Wahyu Saputro, S.Ag",
+		"email"	: "ijonk@rauzuran.com",
+		"web"	: "www.ijonk-uwu.com"
+	},
+	
+	{
+		"nip"	: "983406268",
+		"nama"	: "Ava Adam, M.Pd",
+		"email"	: "avadam@rauzuran.com",
+		"web"	: "www.avanesse.com"
+	},
+	
+	{
+		"nip"	: "983447868",
+		"nama"	: "Marsahall Cendikiawan, M.Pd",
+		"email"	: "marshall@rauzuran.com",
+		"web"	: "www.marshall-cend.com"
+	},
+	
+	{
+		"nip"	: "958447868",
+		"nama"	: "Dr. Herpinus",
+		"email"	: "dr.herp@rauzuran.com",
+		"web"	: "www.herp-pinus.com"
+	},
+	
+	{
+		"nip"	: "983887868",
+		"nama"	: "Bosendi Megalodon, M.Pd",
+		"email"	: "senidbos@rauzuran.com",
+		"web"	: "www.tavvoranboz-sendi.com"
+	},
+	
+	{
+		"nip"	: "983449968",
+		"nama"	: "Yayan Ruhiyan, M.Pd",
+		"email"	: "omyayan@rauzuran.com",
+		"web"	: "www.dodogmadog.com"
+	},
+	
+	{
+		"nip"	: "989047868",
+		"nama"	: "Chuck Norrris, M.Pd",
+		"email"	: "noris-chuck@rauzuran.com",
+		"web"	: "www.noris-anti.com"
+	},
+	
+	{
+		"nip"	: "983967868",
+		"nama"	: "Aufa Nurul Awwal, M.Pd",
+		"email"	: "aufana@rauzuran.com",
+		"web"	: "www.aufawwal.com"
+	},
+	
+	{
+		"nip"	: "909447868",
+		"nama"	: "Stenly Stooner, M.Pd",
+		"email"	: "stenly@rauzuran.com",
+		"web"	: "www.stoner.com"
+	},
+	
+	{
+		"nip"	: "965847868",
+		"nama"	: "Derpina Rahmawati, M.Pd",
+		"email"	: "derpina@rauzuran.com",
+		"web"	: "www.derpin-rahm.com"
+	},
+	
+	{
+		"nip"	: "980987868",
+		"nama"	: "Ajitama Nurul Awwal, M.Pd",
+		"email"	: "marshall@rauzuran.com",
+		"web"	: "www.marshall-cend.com"
+	},
+	
+	{
+		"nip"	: "909447868",
+		"nama"	: "Sandi Sundoro, M.Pd",
+		"email"	: "ndorohits@rauzuran.com",
+		"web"	: "www.sundoro-artes.com"
+	},
+	
+	{
+		"nip"	: "9094400968",
+		"nama"	: "Riski Banter, M.Pd",
+		"email"	: "riskib@rauzuran.com",
+		"web"	: "www.roller-balap.com"
+	},
+	
+	{
+		"nip"	: "909877868",
+		"nama"	: "Kamto Berkah, M.Pd",
+		"email"	: "be-kam@rauzuran.com",
+		"web"	: "www.ber-kam.com"
+	},
+	
+	{
+		"nip"	: "906787868",
+		"nama"	: "Suharto Petrus, M.Pd",
+		"email"	: "hartrus@rauzuran.com",
+		"web"	: "www.suhar-trus.com"
+	},
+	
+	{
+		"nip"	: "991793868",
+		"nama"	: "Aufa Yogharino, M.Pd",
+		"email"	: "aufays@rauzuran.com",
+		"web"	: "www.aufay.com"
+	},
+	
+	{
+		"nip"	: "909448768",
+		"nama"	: "Romualdus Sundoro, M.Pd",
+		"email"	: "romualdus@rauzuran.com",
+		"web"	: "www.ndoromualdus.com"
+	},
+	
 
 ];
 
